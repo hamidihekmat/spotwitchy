c8455439fe82d21bd4aefb412be20b862247060c
Hekmat Hamidi <theonehamidi@gmail.com>
2022-04-20 13:27:21 -0400

    added secret as env

--- upstash/index.ts
+++ upstash/index.ts
@@ -1,7 +1,6 @@
 import { Redis } from '@upstash/redis';
 
 export const redis = new Redis({
-  url: 'https://us1-tight-chicken-34551.upstash.io',
-  token:
-    'AYb3ASQgOTUwMjYxNDQtNTk3MS00NjZlLTllZjUtZTA0YTI5OTFlNmVhNWVhM2ZiMzFmN2U0NGU3YTgxODcxMzI5NjQ2ODAyYzk=',
+  url: process.env.UPSTASH_REDIS_URL!,
+  token: process.env.UPSTASH_REDIS_TOKEN!,
 });
