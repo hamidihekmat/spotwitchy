6742b157c449d3efefec76f9b2361174f27c16bc
Daniel Cornelison <itsklastic@gmail.com>
2022-05-22 19:01:40 -0500

    style: quotes

--- pages/preview.tsx
+++ pages/preview.tsx
@@ -1,17 +1,17 @@
-import { Leva } from "leva";
-import { NextPage } from "next";
-import { useEffect } from "react";
-import { SpotifyWidget } from "../components/spotify";
-import { useSpotifyConfig } from "../components/spotify/config";
-import ErrorBoundry from "../components/ErrorBoundary";
+import { Leva } from 'leva';
+import { NextPage } from 'next';
+import { useEffect } from 'react';
+import { SpotifyWidget } from '../components/spotify';
+import { useSpotifyConfig } from '../components/spotify/config';
+import ErrorBoundry from '../components/ErrorBoundary';
 
 const useSetTransparent = () => {
   useEffect(() => {
-    const body = document.getElementsByTagName("body");
-    const html = document.getElementsByTagName("html");
+    const body = document.getElementsByTagName('body');
+    const html = document.getElementsByTagName('html');
 
-    (body[0].style.background = "transparent"),
-      (html[0].style.backgroundColor = "transparent");
+    (body[0].style.background = 'transparent'),
+      (html[0].style.backgroundColor = 'transparent');
   });
 };
 
